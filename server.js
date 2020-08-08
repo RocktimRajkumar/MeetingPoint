e65ce9b7313fcb850666899a6f3ff2279e8e6176
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2020-08-08 21:51:05 +0530

    feat: Added ability to view our own video

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid');
 
+// Setting view engine to ejs
 app.set('view engine', 'ejs');
+// Setting the public folder
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
